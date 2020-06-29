85b472631f46dac51456a3f07fb087c4ad7b6bc1
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-06-30 01:26:50 +0800

    added some legends

--- lib/main.dart
+++ lib/main.dart
@@ -110,20 +110,81 @@ class _AppBodyState extends State<AppBody> {
                     )
                   ],
                 ),
+                SizedBox(height: 50.0),
                 Row(
                   children: <Widget>[
                     Expanded(
                       child: Container(
                         margin: const EdgeInsets.all(10.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: "Height (cm)",
-                              hintText: "Eg: 159.2",
-                              alignLabelWithHint: false,
-                              filled: true),
-                          keyboardType: TextInputType.number,
-                          textInputAction: TextInputAction.done,
+                        child: RichText(
+                          text: TextSpan(
+                              text: 'Legends:\n',
+                              style: DefaultTextStyle.of(context).style,
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: '• Less than',
+                                ),
+                                TextSpan(
+                                    text: ' 16.0 - ',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                                TextSpan(
+                                  text: 'Severely Underweight\n',
+                                ),
+                                TextSpan(
+                                  text: '• From',
+                                ),
+                                TextSpan(
+                                    text: ' 16.0 to 18.5 - ',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                                TextSpan(
+                                  text: 'Underweight\n',
+                                ),
+                                TextSpan(
+                                  text: '• From',
+                                ),
+                                TextSpan(
+                                    text: ' 18.6 to 25.0 - ',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                                TextSpan(
+                                  text: 'Normal Weight\n',
+                                ),
+                                TextSpan(
+                                  text: '• From',
+                                ),
+                                TextSpan(
+                                    text: ' 25.1 to 30.0 - ',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                                TextSpan(
+                                  text: 'Overweight\n',
+                                ),
+                                TextSpan(
+                                  text: '• From',
+                                ),
+                                TextSpan(
+                                    text: ' 30.1 to 35.0 - ',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                                TextSpan(
+                                  text: 'Moderately Obese\n',
+                                ),
+                                TextSpan(
+                                  text: '• More than',
+                                ),
+                                TextSpan(
+                                    text: ' 35.0 - ',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                                TextSpan(
+                                  text: 'Severely Obese\n',
+                                ),
+                                TextSpan(
+                                  text: 'Wikipedia',
+                                ),
+                              ]),
                         ),
                       ),
                     )
@@ -137,3 +198,6 @@ class _AppBodyState extends State<AppBody> {
     );
   }
 }
+
+//TODO: letak share icon kat toolbar
+//TODO: Letak calculate/reset button
